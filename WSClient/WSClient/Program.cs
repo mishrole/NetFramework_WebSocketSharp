bde4725a79f6d5391600e7ea6e3fa047605eb35c
Mitchell Rodríguez <mitrodle@gmail.com>
2022-08-26 16:15:46 -0500

    ADD: Basic Client & Server WS using WebSocketSharp

---
+++ WSClient/WSClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using WebSocketSharp;
+
+namespace WSClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // WS client instance
+            using (WebSocket wsc = new WebSocket("ws://127.0.0.1:9090/Echo"))
+            {
+                // Add Handler to listen
+                wsc.OnMessage += WS_AnswerToEmitter;
+                // Connect to server
+                wsc.Connect();
+                // Send data to Server
+                wsc.Send("Hello WS Server!");
+                Console.WriteLine("Client: 'Hello WS Server' sended to Server");
+
+                // Close console on key pressed
+                Console.ReadKey();
+            }
+
+        }
+
+        private static void WS_AnswerToEmitter(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Client: Message received from server is -> '" + e.Data + "'");
+        }
+    }
+}
---
+++ WSServer/WSServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WSServer
+{
+    // Custome implementation of WSBehavior
+    public class Echo : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Server: Received message from client is -> '" + e.Data + "'");
+            // Send message to the server
+            Send("Hi, I'm a server. I received your message '" + e.Data + "'");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WS server instance
+            WebSocketServer wssv = new WebSocketServer("ws://127.0.0.1:9090");
+
+            // WS Service Route with Echo WS Behavior
+            wssv.AddWebSocketService<Echo>("/Echo");
+
+            // Start Server
+            wssv.Start();
+
+            Console.WriteLine("Server: started on ws://127.0.0.1:9090/Echo");
+
+            // Stop server on key pressed
+            Console.ReadKey();
+            wssv.Stop();
+        }
+    }
+}
